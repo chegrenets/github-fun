6d9f7272cda40e36062455116e85634a873e4296
Oleksandr Chehrenets <alex.chegrenets@gmail.com>
2019-02-22 12:29:18 +0200

    7777777

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -6,11 +6,16 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("M1");
+            Console.WriteLine("M0s");
             Console.WriteLine("M2");
             Console.WriteLine("M3");
             Console.WriteLine("M4");
             Console.WriteLine("M5");
+
+            Console.WriteLine("M6");
+
+            Console.WriteLine("M7");
+
         }
     }
 }
