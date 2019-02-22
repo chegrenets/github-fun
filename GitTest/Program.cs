5a45db9261e2c672a2751c309972c8a4abe454aa
Oleksandr Chehrenets <alex.chegrenets@gmail.com>
2019-02-22 12:59:26 +0200

    Base

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -6,7 +6,7 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("M0s");
+            Console.WriteLine("M1");
             Console.WriteLine("M2");
             Console.WriteLine("M3");
             Console.WriteLine("M4");
