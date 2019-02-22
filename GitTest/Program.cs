e52d00b02972a00f88b47c82705132de6b1b15ed
Oleksandr Chehrenets <alex.chegrenets@gmail.com>
2019-02-22 14:09:40 +0200

    bugfix_total

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -24,6 +24,8 @@ namespace GitTest
             Console.WriteLine("feature2_1_bugfix");
             Console.WriteLine("feature2_2");
 
+            Console.WriteLine("total_bugfix");
+
 
         }
     }
