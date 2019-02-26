1818c981137d4e867fd5ff0eeaebb85a4de5c7c1
Oleksandr Chehrenets <alex.chegrenets@gmail.com>
2019-02-26 13:14:20 +0200

    base

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -6,6 +6,7 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
+            // =============== BASE ===================
             Console.WriteLine("M0");
             Console.WriteLine("M1");
             Console.WriteLine("M2");
@@ -17,46 +18,29 @@ namespace GitTest
             Console.WriteLine("M8");
             Console.WriteLine("M9_bugfix2");
             Console.WriteLine("M10");
-
             Console.WriteLine("feature1_1");
             Console.WriteLine("feature1_2");
-
             Console.WriteLine("feature2_1_bugfix");
             Console.WriteLine("feature2_2");
-
             Console.WriteLine("total_bugfix");
-
             Console.WriteLine("feature3_1");
             Console.WriteLine("feature3_2");
             Console.WriteLine("feature3_3");
-            
-
             Console.WriteLine("feature4_1");
-            
             Console.WriteLine("feature5_master");            
             Console.WriteLine("feature5_v3");
-
             Console.WriteLine("feature6_local_v1");
             Console.WriteLine("feature6_local_v2");
-
-
             Console.WriteLine("feature7_local_v1");
             Console.WriteLine("feature7_local_v2");
             Console.WriteLine("feature7_remote_v1");
             Console.WriteLine("feature7_remote_v2");
-
             Console.WriteLine("feature8_local_v1");
             Console.WriteLine("feature8_local_v2");
-
             Console.WriteLine("feature8_remote_v1");
             Console.WriteLine("feature9_remote_v1");
-
-
             Console.WriteLine("feature9_local_v1");
-
-            
-
-
+            // =============== BASE ==================
 
         }
     }
